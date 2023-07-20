d97c3be8240f749ddfbccf6e46d2cdc4a43cc699
embet-mengistu <131568421+embet-mengistu@users.noreply.github.com>
2023-07-20 13:48:27 -0400

    env added

--- src/Pages/Home/AnswerPage/Answer.js
+++ src/Pages/Home/AnswerPage/Answer.js
@@ -26,13 +26,16 @@ function AnswerPage({ allQuestions }) {
     const fetchQuestions = async () => {
       try {
         // //fetching the questions from data base (getPosts(found in question.controller))
-        const questionRes = await fetch("http://localhost:3000/api/questions", {
-          method: "GET",
-          // ///needs auth
-          headers: {
-            "x-auth-token": userData.token,
-          },
-        });
+        const questionRes = await fetch(
+          `${process.env.REACT_APP_base_url}/api/questions`,
+          {
+            method: "GET",
+            // ///needs auth
+            headers: {
+              "x-auth-token": userData.token,
+            },
+          }
+        );
 
         if (!questionRes.ok) {
           throw new Error("Network response was not ok");
@@ -77,7 +80,7 @@ function AnswerPage({ allQuestions }) {
     try {
       const answerRes = await axios.get(
         // /sending the question id as a params(url)
-        `http://localhost:3000/api/answer/${filteredquestionId}`,
+        `${process.env.REACT_APP_base_url}/api/answer/${filteredquestionId}`,
         {
           headers: { "x-auth-token": userData.token },
         }
@@ -111,7 +114,7 @@ function AnswerPage({ allQuestions }) {
     e.preventDefault();
     try {
       const loginRes = await axios.post(
-        "http://localhost:3000/api/answer/postanswer",
+        `${process.env.REACT_APP_base_url}/api/answer/postanswer`,
         {
           answerDescription: form.answerDescription,
           questionId: filteredquestionId,
